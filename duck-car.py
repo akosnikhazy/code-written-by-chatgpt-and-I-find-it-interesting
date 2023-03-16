6e2c9ec46a515c8c72f06391dae2f5e3da29c5ee
Ákos Nikházy <nikhazyakos@tutanota.com>
2023-03-16 09:45:30 +0100

    Create duck-car.py

---
+++ duck-car.py
@@ -0,0 +1,33 @@
+# Here I just wanted to see what GPT-3 does with nonsense
+# prompt:
+# The duck car giraffe but then horse program python C#. Why X is the worst and lion is the Rust. In English list those that makes blob to la li lu le lo. If true how big is it and why? 
+#
+# response: 
+# I'm sorry, but the statement you provided does not make any sense or have a clear meaning. It appears to be a random collection of words and phrases that do not form a coherent sentence or question.
+# If you have a specific question or topic that you would like me to help you with, please feel free to ask and I will do my best to provide a helpful response.
+#
+# prompt: 
+# It makes sense, you can write a poem or code about it
+#
+# response: 
+# [here it wrote a poem and then] As for code, I can provide an example Python program that incorporates some of the phrases you provided:
+
+# Define a class for a duck car
+class DuckCar:
+    def __init__(self):
+        self.speed = 50
+        self.color = "yellow"
+        self.sound = "quack quack"
+    
+    def drive(self):
+        print("The duck car drives with ease.")
+    
+    def honk(self):
+        print(self.sound)
+    
+# Create an instance of the DuckCar class
+my_car = DuckCar()
+
+# Call the drive and honk methods
+my_car.drive()
+my_car.honk()
